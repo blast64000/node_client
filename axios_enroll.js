86f6459de104a47eca7afd1f4866bef6032de8b8
John Kim at Work <blast64000@icloud.com>
2021-02-16 18:02:55 +0900

    ss

--- axios_enroll.js
+++ axios_enroll.js
@@ -17,10 +17,10 @@ axios
             },
 
             params: {
-                name: 'Buy the milk_01',
-                photoUrl: 'c-sg.smule.com/rs-s27/arr/c6/05/1db60541-5165-4235-9672-e22399291d1f_256.jpg',
-                description: 'api request test',
-                managers: 'blast64000@algenbio'
+                "name": "Buy the milk_01",
+                "photoUrl": "c-sg.smule.com/rs-s27/arr/c6/05/1db60541-5165-4235-9672-e22399291d1f_256.jpg",
+                "description": "api request test",
+                "managers": ["blast64000@algenbio"]
             },
 
 
