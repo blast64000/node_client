89f274d325488d0d17eb34a5781f6ac5f3084867
John Kim at Work <blast64000@icloud.com>
2021-02-19 10:01:04 +0900

    add header

--- del_bot.js
+++ del_bot.js
@@ -9,20 +9,18 @@ axios
              "description": "api modify test",
              "managers": ["blast64000@algenbio"]
          },
+        */
 
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                consumerKey: 'BHOjH7zxMnPPqXwycpf8',
+                Authorization: 'Bearer AAABBju93cZn+gX41vXE5cObvYbvknsyM8CuLELT+CnoZQYueIYcuNdk7rijOR4l/YloZEX6WIUUfA1IsxUVIrwEm4irCeApyV0K/7qz/y7OkMywgv6C4E7saVN8wIOZ2T7JZbYN2XAoLrZOQhLQpV0SBtvHrWaUJfgLVucq8u3efGnREXOtTdS3PrbVeNQVSD/6a3kFcy7FCjTVWFRa8gHTFURBP2L6VRm8+50O3tmPvEFO5ufMechfBoSUXxKjoRgWnyTpzVwhpY0dYuryMGE/l4XSS0Q7NAkg1FSV05MB7wWa0vZEatWOBX4oUq+94FJAXsxfog2V0tAcRotPeluouoDfHJESnK3pD9PAHBI8A7Km'
+            }
 
-         {
-             headers: {
-                 'Content-Type': 'application/json',
-                 consumerKey: 'BHOjH7zxMnPPqXwycpf8',
-                 Authorization: 'Bearer AAABBju93cZn+gX41vXE5cObvYbvknsyM8CuLELT+CnoZQYueIYcuNdk7rijOR4l/YloZEX6WIUUfA1IsxUVIrwEm4irCeApyV0K/7qz/y7OkMywgv6C4E7saVN8wIOZ2T7JZbYN2XAoLrZOQhLQpV0SBtvHrWaUJfgLVucq8u3efGnREXOtTdS3PrbVeNQVSD/6a3kFcy7FCjTVWFRa8gHTFURBP2L6VRm8+50O3tmPvEFO5ufMechfBoSUXxKjoRgWnyTpzVwhpY0dYuryMGE/l4XSS0Q7NAkg1FSV05MB7wWa0vZEatWOBX4oUq+94FJAXsxfog2V0tAcRotPeluouoDfHJESnK3pD9PAHBI8A7Km'
-             }
-
-         }*/
+        }
 
     )
-
-
 
 .then(res => {
         console.log(`statusCode: ${res.statusCode}`)
