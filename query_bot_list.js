ac3b090275c0cc9cad7b9c51a1ca183c6e9c2c1c
John Kim at Work <blast64000@icloud.com>
2021-02-19 16:05:20 +0900

    rr

--- query_bot_list.js
+++ query_bot_list.js
@@ -18,7 +18,6 @@ axios
                 consumerKey: 'BHOjH7zxMnPPqXwycpf8',
                 Authorization: 'Bearer AAABBju93cZn+gX41vXE5cObvYbvknsyM8CuLELT+CnoZQYueIYcuNdk7rijOR4l/YloZEX6WIUUfA1IsxUVIrwEm4irCeApyV0K/7qz/y7OkMywgv6C4E7saVN8wIOZ2T7JZbYN2XAoLrZOQhLQpV0SBtvHrWaUJfgLVucq8u3efGnREXOtTdS3PrbVeNQVSD/6a3kFcy7FCjTVWFRa8gHTFURBP2L6VRm8+50O3tmPvEFO5ufMechfBoSUXxKjoRgWnyTpzVwhpY0dYuryMGE/l4XSS0Q7NAkg1FSV05MB7wWa0vZEatWOBX4oUq+94FJAXsxfog2V0tAcRotPeluouoDfHJESnK3pD9PAHBI8A7Km'
             }
-
         })
 
 
