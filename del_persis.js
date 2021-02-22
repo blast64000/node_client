535806e952316feb25c23833b2f760acfe63033e
John Kim at Work <blast64000@icloud.com>
2021-02-22 09:53:54 +0900

    make delete code

--- del_persis.js
+++ del_persis.js
@@ -1,15 +1,14 @@
 const axios = require('axios')
 
 axios
-    .get('https://apis.worksmobile.com/r/kr1unqNPDxwAo/message/v1/bot/1909195/persistentmenu',
+    .delete('https://apis.worksmobile.com/r/kr1unqNPDxwAo/message/v1/bot/1913014/persistentmenu',
 
-        /*
-        {
-            "name": "Buy the milk_02",
-            "photoUrl": "https://c-sg.smule.com/rs-s27/arr/c6/05/1db60541-5165-4235-9672-e22399291d1f_256.jpg",
-            "description": "api modify test",
-            "managers": ["blast64000@algenbio"]
-        },
+        /* {
+             "name": "Buy the milk_02",
+             "photoUrl": "https://c-sg.smule.com/rs-s27/arr/c6/05/1db60541-5165-4235-9672-e22399291d1f_256.jpg",
+             "description": "api modify test",
+             "managers": ["blast64000@algenbio"]
+         },
         */
 
         {
@@ -18,14 +17,13 @@ axios
                 consumerKey: 'BHOjH7zxMnPPqXwycpf8',
                 Authorization: 'Bearer AAABBju93cZn+gX41vXE5cObvYbvknsyM8CuLELT+CnoZQYueIYcuNdk7rijOR4l/YloZEX6WIUUfA1IsxUVIrwEm4irCeApyV0K/7qz/y7OkMywgv6C4E7saVN8wIOZ2T7JZbYN2XAoLrZOQhLQpV0SBtvHrWaUJfgLVucq8u3efGnREXOtTdS3PrbVeNQVSD/6a3kFcy7FCjTVWFRa8gHTFURBP2L6VRm8+50O3tmPvEFO5ufMechfBoSUXxKjoRgWnyTpzVwhpY0dYuryMGE/l4XSS0Q7NAkg1FSV05MB7wWa0vZEatWOBX4oUq+94FJAXsxfog2V0tAcRotPeluouoDfHJESnK3pD9PAHBI8A7Km'
             }
-        })
+        }
 
+    )
 
 .then(res => {
         console.log(`statusCode: ${res.statusCode}`)
         console.log(res)
-        console.log(res.data.content)
-
     })
     .catch(error => {
         console.error(error)
