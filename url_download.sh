594299088f2aba216565cf908b9d68d8acdb427e
John Kim at Work <blast64000@icloud.com>
2021-02-22 15:15:35 +0900

    ddd

--- url_download.sh
+++ url_download.sh
@@ -2,5 +2,5 @@ curl -XPOST "http://storage.worksmobile.com/openapi/message/upload.api" \
          -H "consumerKey:BHOjH7zxMnPPqXwycpf8" \
          -H "authorization: Bearer AAABBju93cZn+gX41vXE5cObvYbvknsyM8CuLELT+CnoZQYueIYcuNdk7rijOR4l/YloZEX6WIUUfA1IsxUVIrwEm4irCeApyV0K/7qz/y7OkMywgv6C4E7saVN8wIOZ2T7JZbYN2XAoLrZOQhLQpV0SBtvHrWaUJfgLVucq8u3efGnREXOtTdS3PrbVeNQVSD/6a3kFcy7FCjTVWFRa8gHTFURBP2L6VRm8+50O3tmPvEFO5ufMechfBoSUXxKjoRgWnyTpzVwhpY0dYuryMGE/l4XSS0Q7NAkg1FSV05MB7wWa0vZEatWOBX4oUq+94FJAXsxfog2V0tAcRotPeluouoDfHJESnK3pD9PAHBI8A7Km" \
          -H "x-works-apiid:kr1unqNPDxwAo" \
-         -H "x-works-resource-id:WAAAQLrbUHwtPzJkJ55S5dg1oww2fiFLeKKPN5WuW8TIMbIslREut7BiAzkv+WszjPRqcMdEjXdqGjVsZdkRd/mrVHs="\
+         -H "x-works-resource-id:XAAAUFu0VclmHteAMq0SJrW1/4DBkYiobjWh9Hijhg1FJKP9rlOMqUr0APiEcWucIIcMuVN1z6fuo72wL+xCRNu/iLHG/3fGYJuO+d62TJwb61cq"\
          -o "./picture_download.png"
