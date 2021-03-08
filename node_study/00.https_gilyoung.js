b73e186f95bb3ab909d14a4446e4ed5dafccb8a1
John Kim at Work <blast64000@icloud.com>
2021-03-08 15:34:01 +0900

    dddd

--- node_study/00.https_gilyoung.js
+++ node_study/00.https_gilyoung.js
@@ -7,6 +7,7 @@ const options = {
     push_url: 'https://apis.worksmobile.com/r/kr1unqNPDxwAo/message/v1/bot/1937543/message/push',
 
     headers: {
+        charset = 'UTF-8',
         'Content-Type': 'application/json',
         consumerKey: 'BHOjH7zxMnPPqXwycpf8',
         Authorization: 'Bearer AAABAoyo/3zFAMqau9uS6fIR0pPRf2z8FkHbsXa930xScg8gYjguZ81L0S8AszS7flys2lIznE4VfF8UZjFLDjhUHdNkwgYtdHPGpp3PaXc7iyHUtpkZFs9Y3wzS6zhML6zQKNu//940sNw9VqjFkKuulbAoTAF9xsJ7PA0r6OrhaxTzp/cWzEKFTmn81+MBpBvBPPL9N+EcbL+YyQiny+2gV1fHdRQRA2fVO9tiwLkHuLLPVEnpp6BJBfDGKIGy1GUIYiHA0xuEIBMl5vYoVwedo25HpnUhnWIro+GBm1Jhqn3sA6whkep7xsWFxD944TZ7IwiM6i3ipTXVfYppnUwdDINHFBDcTtX/11ienn8E/LRU'
@@ -60,6 +61,7 @@ function onRequest(req, res) {
 
         }, function(err, response, body) {
             if (err) {
+                console.log('========= enter error ========.');
                 console.error(err);
             } else {
                 console.info(body);
