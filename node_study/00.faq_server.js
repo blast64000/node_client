44c881e75b7e937cc1173d47cf7a5da038709ffd
blast64000 <blast64000@icloud.com>
2021-03-16 01:30:43 +0900

    ㅇㅇㅇ

--- node_study/00.faq_server.js
+++ node_study/00.faq_server.js
@@ -122,7 +122,7 @@ function onRequest(req, res) {
                     }
                 }
             }
-            else if (reqBody.content.text==='시작하기'){
+            else if (reqBody.content.postback==='시작하기'){
                 reqBody.content.text = '안녕하세요 회사생활관련 궁금한 부문을 찾아드리는 문의봇입니다.';
                 url_link = 'https://apis.worksmobile.com/r/kr1unqNPDxwAo/message/v1/bot/1937543/message/push';
                 var reqBody = {
@@ -145,7 +145,7 @@ function onRequest(req, res) {
                           }, {
                             "type": "message",
                             "label": "우수인재 Pool 및 사내추천",
-                            "postback": "사내추천"
+                            "postback": "사내추천00"
                           }, {
                             "type": "message",
                             "label": "기타",
@@ -155,6 +155,241 @@ function onRequest(req, res) {
                     }
                 }
             }
+//사내추천
+            else if (reqBody.content.postback==='사내추천00'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '사내직원이 직접 대웅에 가장 FIT한 우수인재를 추천함으로써 우수인재를 확보하는 채용제도입니다.',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천01"
+                          }, {
+                            "type": "message",
+                            "label": "처음으로 돌아가기",
+                            "postback": "시작하기"
+                          }
+                        ]
+                    }
+                }
+            }
+            else if (reqBody.content.postback==='사내추천01'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '추천가능한 우수인재 기준은 경력 3년이상입니다. (단, 로컬영업의 경우 1년 이상부터 추천 가능)',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천02"
+                          }
+                        ]
+                    }
+                }
+            }
+            else if (reqBody.content.postback==='사내추천02'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '추천된 우수인재는 인사팀에서 검토후 지속 관리되며 채용으로 연결될시 입사시와 1년근속 평가에따라 추천자에게 포상을 지급합니다.',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천03"
+                          }
+                        ]
+                    }
+                }
+            }
+            else if (reqBody.content.postback==='사내추천03'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '추천할 우수인재가 있나요?',
+                        actions: [{
+                            "type": "message",
+                            "label": "네",
+                            "postback": "사내추천04y"
+                          }, {
+                            "type": "message",
+                            "label": "아니오",
+                            "postback": "사내추천04n"
+                          }
+                        ]
+                    }
+                }
+            }
+            else if (reqBody.content.postback==='사내추천04n'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '주변에 우수인재가 있다면 적극적으로 추천해주시기 바랍니다 ^^',
+                        actions: [{
+                            "type": "message",
+                            "label": "처음으로 돌아가기",
+                            "postback": "시작하기"
+                          }
+                        ]
+                    }
+                }
+            }
+
+            else if (reqBody.content.postback==='사내추천04y'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '아래 우수인재 기준에 부합하는지 검토해주세요.',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천05"
+                          }
+                        ]
+                    }
+                }
+            }
+
+            else if (reqBody.content.postback==='사내추천05'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: ' 1. 대웅의 인재상에 맞는 사람\n     - 끊임없이 학습하여 변화하고 이를 업무에 적용할 수 있는 사람\n     - 열린 마음으로 상사/동료와 소통/협력하여 더 좋은 성과를 이끌어 낼 수 있는 사람\n     - 자율과 몰입을 통해 하기로 한 일은 끝까지 해내는 사람\n',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천06"
+                          }
+                        ]
+                    }
+                }
+            }
+
+            else if (reqBody.content.postback==='사내추천06'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '  2. 동일/유사 업무에 전문성을 지닌 사람\n      - 경력 : 3년이상 경력자 \n                  (※ 로컬영업의 경우, 경력 1년이상부터 추천 가능)\n      - 구체적인 성공 사례(성과)가 있는 사람\n         예) 본인의 직무/업무와 관련하여 고성과를 낸 사례\n              일하는 방법의 개선을 통해 주변 동료들에게 영향력을 준 사례 \n',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천07"
+                          }
+                        ]
+                    }
+                }
+            }
+
+            else if (reqBody.content.postback==='사내추천07'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '추천서 양식을 작성후 인사팀에 메일을 보내주세요. \n인사팀 검토후, 우수인재가 맞다면 대상자에게 대웅의 문화와 비전 및 채용소식을 정기적으로 보내드립니다. \n인재Pool 등록시 스타벅스 아메리카노를 기프티콘으로 발송드립니다.',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천08"
+                          }
+                        ]
+                    }
+                }
+            }
+            
+
+            else if (reqBody.content.postback==='사내추천08'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '이후, 적합한 포지션에대해 지원자가 포지션에 지원하여 채용이 되었다면, 추천자에게 포상이 지급됩니다.',
+                        actions: [{
+                            "type": "message",
+                            "label": "다음",
+                            "postback": "사내추천09"
+                          }
+                        ]
+                    }
+                }
+            }
+
+            else if (reqBody.content.postback==='사내추천09'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '추가로 궁금하신 사항이 있으신가요?',
+                        actions: [{
+                            "type": "message",
+                            "label": "추천자포상",
+                            "postback": "사내추천10"
+                          },
+                          
+                          {
+                            "type": "message",
+                            "label": "담당자 연락하기",
+                            "postback": "담당자"
+                          },
+                          
+                          {
+                            "type": "message",
+                            "label": "진행시 주의할 사항",
+                            "postback": "사내추천11"
+                          },
+
+                          {
+                            "type": "message",
+                            "label": "처음으로 돌아가기",
+                            "postback": "시작하기"
+                          }
+                        ]
+                    }
+                }
+            }
+
+            else if (reqBody.content.postback==='사내추천10'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '그림(인사팀 이미지 요청중)',
+                        actions: [{
+                            "type": "message",
+                            "label": "이전으로",
+                            "postback": "사내추천09"
+                          }
+                        ]
+                    }
+                }
+            }
+            else if (reqBody.content.postback==='사내추천11'){
+                var reqBody = {
+                    accountId: parsedBody.source.accountId,
+                    content: {
+                        type: 'button_template',
+                        contentText: '① 우수인재 Pool 사전 등록 후 채용까지 히스토리 관리된 경우만 인정 (입사 후 적용 X)\n② 추천대상자 : 채용사이트 이력서 등록시 추천인 정보 입력\n',
+                        actions: [{
+                            "type": "message",
+                            "label": "이전으로",
+                            "postback": "사내추천09"
+                          }
+                        ]
+                    }
+                }
+            }
+
+
+
+
+//경력채용
             else if (reqBody.content.postback==='경력채용00'){
                 var reqBody = {
                     accountId: parsedBody.source.accountId,
@@ -165,10 +400,6 @@ function onRequest(req, res) {
                             "type": "message",
                             "label": "다음",
                             "postback": "경력채용01"
-                          }, {
-                            "type": "message",
-                            "label": "처음으로 돌아가기",
-                            "postback": "시작하기"
                           }
                         ]
                     }
