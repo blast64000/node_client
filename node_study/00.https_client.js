30c8e1e2bcf9494204b3a2a4fe3a817ada8cf6cf
John Kim at Work <blast64000@icloud.com>
2021-04-21 17:34:35 +0900

    add_웅디

--- node_study/00.https_client.js
+++ node_study/00.https_client.js
@@ -8,11 +8,15 @@ var options = {
     method: 'GET',
     path: '/r/kr1unqNPDxwAo/message/v1/bot',
 
+
+
+
     headers: {
         'Content-Type': 'application/json',
         consumerKey: 'BHOjH7zxMnPPqXwycpf8',
         Authorization: 'Bearer AAABAoyo/3zFAMqau9uS6fIR0pPRf2z8FkHbsXa930xScg8gYjguZ81L0S8AszS7flys2lIznE4VfF8UZjFLDjhUHdNkwgYtdHPGpp3PaXc7iyHUtpkZFs9Y3wzS6zhML6zQKNu//940sNw9VqjFkKuulbAoTAF9xsJ7PA0r6OrhaxTzp/cWzEKFTmn81+MBpBvBPPL9N+EcbL+YyQiny+2gV1fHdRQRA2fVO9tiwLkHuLLPVEnpp6BJBfDGKIGy1GUIYiHA0xuEIBMl5vYoVwedo25HpnUhnWIro+GBm1Jhqn3sA6whkep7xsWFxD944TZ7IwiM6i3ipTXVfYppnUwdDINHFBDcTtX/11ienn8E/LRU'
     },
+
     key: fs.readFileSync('/etc/letsencrypt/archive/herb-cookie.com/privkey1.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/archive/herb-cookie.com/cert1.pem'),
     ca: fs.readFileSync('/etc/letsencrypt/archive/herb-cookie.com/chain1.pem')
