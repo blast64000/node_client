2996d63387696f6aaea84c5fa04a4b9aa5c77809
John Kim at Work <blast64000@icloud.com>
2021-06-22 16:53:04 +0900

    dddddd

--- node_study/app.js
+++ node_study/app.js
@@ -25,8 +25,10 @@ async function asyncFunction() {
         const botMaster = await conn.query(`select * from chatbot.bot_ms_tb where BOT_USE_ST=1`);
         var iter;
         var qinfo = "CONT_BOT_CD IN (";
+        console.log(botMaster.length)
         for (iter = 0; iter < botMaster.length; iter++) {
             qinfo += botMaster[iter].BOT_CD
+            qinfo += ','
         };
         qinfo += ')';
         console.log(qinfo);
