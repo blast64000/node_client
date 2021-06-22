4039c7affd8b511896d39013acc5c527612cd599
John Kim at Work <blast64000@icloud.com>
2021-06-22 16:13:48 +0900

    dddd

--- node_study/app.js
+++ node_study/app.js
@@ -13,27 +13,45 @@ const pool = mariadb.createPool({
     connectionLimit: 5
 });
 
-//2. 링크드 리스트 생성 
+var dbload_info = {
+    accountId: parsedBody.source.accountId,
+    content: {
+        type: parsedBody.content.type,
+        text: parsedBody.content.text,
+        postback: parsedBody.content.postback
+    },
+
+}
+
 async function asyncFunction() {
     let conn;
     try {
         conn = await pool.getConnection();
-        const rows = await conn.query('select 1 as val');
-        //console.log(rows);
-        const res = await conn.query("select * from chatbot.cont_ms_tb");
-        //console.log(res);
+
+        // 활성화된 봇 리스트를 긁어옴
+        const botMaster = await conn.query(`select * from chatbot.bot_ms_tb where BOT_USE_ST=1`);
+        console.log(botMaster);
+        const contentMaster = conn.query("select * from chatbot.cont_ms_tb");
+        const actionMaster = conn.query("select * from chatbot.cont_ms_tb");
+
+
+
 
     } catch (err) {
         throw err;
     } finally {
         if (conn) {
             conn.end()
-            return rows;
         };
     }
 }
+
+//1. bot 리스트 읽기
 asyncFunction()
-    //3. 
+    //2. for 문 돌면서 
+
+//2. 링크드 리스트 생성 
+//3. 
 
 
 function onRequest(req, res) {
