7a846d2fa37ea452044ec46e287cb4ecf2c90b0a
John Kim at Work <blast64000@icloud.com>
2021-06-23 10:26:40 +0900

    ddddd

--- node_study/app.js
+++ node_study/app.js
@@ -44,7 +44,8 @@ async function asyncFunction() {
         var iter_;
         var qinfo2 = "CONT_ACT_SET_CD IN (";
         for (iter_ = 0; iter_ < contentMaster.length; iter_++) {
-            qinfo2 += contentMaster[iter_].CONT_ACT_SET_CD
+            qinfo2 += contentMaster[iter_].CONT_ACT_SET_CD;
+            qinfo += ',';
         };
         qinfo2 = qinfo2.slice(0, -2);
         qinfo2 += ')';
