5c8d9560a2f1b01285c6f43a405a298c14f87933
John Kim at Work <blast64000@icloud.com>
2021-06-23 15:02:09 +0900

    dddd

--- node_study/app.js
+++ node_study/app.js
@@ -15,9 +15,10 @@ const pool = mariadb.createPool({
 
 //1. bot 리스트 읽기
 let masterResource = dbconn.readMasterTable().then(function(data) {
-    console.log(masterResource[0]);
-    console.log(masterResource[1]);
-    console.log(masterResource[2]);
+    console.log(masterResource[0][0]);
+    console.log(masterResource[1][0]);
+    console.log(masterResource[2][0]);
+
     https.createServer(conf.options, onRequest).listen(443);
 
 });
