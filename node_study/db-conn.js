716081c87986ef0831bddabcb0660d8db9195699
John Kim at Work <blast64000@icloud.com>
2021-06-23 11:21:35 +0900

    dddd

--- node_study/db-conn.js
+++ node_study/db-conn.js
@@ -35,9 +35,10 @@ async function asyncFunction() {
         const botMaster = await conn.query(`select * from chatbot.bot_ms_tb where BOT_USE_ST=1`);
 
         // 해당하는 봇번호만 추출하여 컨텐츠 긁어오기 
-
+        console.log(makeClause('CONT_BOT_CD', 'BOT_CD', botMaster))
         const contentMaster = await conn.query(`select * from chatbot.cont_ms_tb where ${makeClause('CONT_BOT_CD', 'BOT_CD', botMaster)}`);
-        console.log(contentMaster);
+
+        console.log(makeClause('CONT_ACT_SET_CD', 'CONT_ACT_SET_CD', contentMaster));
         // 해당하는 set 번호가 잇는 추출하여 액션 긁어오기 
         const actionMaster = await conn.query(`select * from chatbot.act_ms_tb where  ${makeClause('CONT_ACT_SET_CD', 'CONT_ACT_SET_CD', contentMaster)}`);
         console.log(actionMaster);
