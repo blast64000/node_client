b166f0ae97ac74cefd08f10929171bb3f2b1dc3e
John Kim at Work <blast64000@icloud.com>
2021-06-23 14:43:37 +0900

    ddddd

--- node_study/db-conn.js
+++ node_study/db-conn.js
@@ -31,15 +31,15 @@ exports.readMasterTable = async function() {
     let conn;
     try {
         conn = await pool.getConnection();
-
         // 활성화된 봇 리스트를 긁어옴
         const botMaster = await conn.query(`select * from chatbot.bot_ms_tb where BOT_USE_ST=1`);
-
         // 해당하는 봇번호만 추출하여 컨텐츠 긁어오기 
         const contentMaster = await conn.query(`select * from chatbot.cont_ms_tb where ${makeClause('CONT_BOT_CD', 'BOT_CD', botMaster)}`);
         // 해당하는 set 번호가 잇는 추출하여 액션 긁어오기 
         const actionMaster = await conn.query(`select * from chatbot.act_ms_tb where  ${makeClause('ACT_SET_CD', 'CONT_ACT_SET_CD', contentMaster)}`);
+        return [botMaster, contentMaster, actionMaster];
     } catch (err) {
+
         throw err;
     } finally {
         if (conn) {
@@ -47,5 +47,4 @@ exports.readMasterTable = async function() {
         };
     }
 
-    return [botMaster, contentMaster, actionMaster];
 }
