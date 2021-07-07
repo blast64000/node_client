526bb89a3b6dc54f8386c2b1d6befab611d1a860
John Kim at Work <blast64000@icloud.com>
2021-07-07 09:49:14 +0900

    ddddd

--- node_study/archive/00.faq_server.js
+++ node_study/archive/00.faq_server.js
@@ -55,27 +55,13 @@ function onRequest(req, res) {
 
 
             if (reqBody.content.text === '시작하기' || reqBody.content.text === '안녕?' || reqBody.content.postback === '시작하기') {
-                reqBody.content.text = '안녕하세요 회사생활관련 궁금한 부문을 찾아드리는 문의봇입니다.';
+                reqBody.content.text = '[행정처분안내]\n 업체명 : 일동제약 \n 처분일자 : 2021-06-25\n 위반내용 : ○ 의약품 ‘다온정’<제5234호>에 대하여 ‘해당 품목 광고업무정지 1개월’(2021. 3. 19. ~ 2021. 4. 18.)의 행정처분을 받았으나, 업무정지기간에 해당 품목을 광고한 사실이 있음\n 처분사항 : ○ 해당 품목 허가취소(2021. 7. 9.자) ';
                 url_link = 'https://apis.worksmobile.com/r/kr1unqNPDxwAo/message/v1/bot/1937543/message/push';
                 var reqBody = {
                     accountId: parsedBody.source.accountId,
                     postback: "",
                     content: {
-                        type: 'button_template',
-                        contentText: '안녕하세요 회사생활관련 궁금한 부문을 찾아드리는 문의봇입니다. 아래 메뉴에서 필요한 항목을 선택해주세요',
-                        actions: [{
-                            "type": "message",
-                            "label": "인사",
-                            "postback": "인사00"
-                        }, {
-                            "type": "message",
-                            "label": "복리후생",
-                            "postback": "복리후생00"
-                        }, {
-                            "type": "message",
-                            "label": "베어월드",
-                            "postback": "베어월드00"
-                        }]
+                        type: 'text'
                     }
                 }
             } else if (reqBody.content.postback === '인사00') {
